e9f7386c5349729a29cf52895ad7e81c3983b03f
Serafim Lazuko <serafim.lazuko@gmail.com>
2019-02-06 23:43:44 +0300

    Added some test-cases

--- StringCalculator/Calculator.Tests/StringCalculatorTest.cs
+++ StringCalculator/Calculator.Tests/StringCalculatorTest.cs
@@ -48,5 +48,29 @@ namespace Calculator.Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Add_ReceivesStringWithSeveralNumbers_ReturnSum()
+        {
+            string argument = "12,1,18";
+
+            int actaul = StringCalculator.Add(argument);
+
+            int expected = 31;
+
+            Assert.AreEqual(expected, actaul);
+        }
+
+        [TestMethod]
+        public void Add_IgnoresVariousNonNumericCharacters_ReturnSum()
+        {
+            string argument = "12a.sw, ///10, od  ??-=_18";
+
+            int actaul = StringCalculator.Add(argument);
+
+            int expected = 40;
+
+            Assert.AreEqual(expected, actaul);
+        }
     }
 }
