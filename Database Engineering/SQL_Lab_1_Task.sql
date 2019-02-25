9a889b5cff7badf4c28c47313f4882f0702407fd
Serafim Lazuka <serafim.lazuko@gmail.com>
2019-02-25 16:43:34 +0300

    Add some quarries

---
+++ Database Engineering/SQL_Lab_1_Task.sql
@@ -0,0 +1,7 @@
+﻿SELECT c.ContactName
+FROM dbo.Customers c 
+INNER JOIN Orders o on c.CustomerID = o.CustomerID
+INNER JOIN [Order Details] od on o.OrderID = od.OrderID
+
+group by c.ContactName 
+Having sum(od.Quantity * od.UnitPrice) > 1000
---
+++ Database Engineering/SQL_Lab_2_Task.sql
@@ -0,0 +1,4 @@
+﻿SELECT Suppliers.CompanyName Supplier, Customers.CompanyName Company, Customers.City City FROM Customers
+inner join Suppliers on Suppliers.City = Customers.City
+AND Suppliers.Country = Customers.Country
+Order By Suppliers.CompanyName
