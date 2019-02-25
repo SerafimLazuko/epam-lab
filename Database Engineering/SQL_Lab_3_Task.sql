89f363bedeeefb74aefaa16e60b794a98192e674
Serafim Lazuka <serafim.lazuko@gmail.com>
2019-02-25 16:51:26 +0300

    DbE 3 task

---
+++ Database Engineering/SQL_Lab_3_Task.sql
@@ -0,0 +1,6 @@
+﻿SELECT c.ContactName
+FROM dbo.Customers c
+INNER JOIN Orders o on c.CustomerID = o.CustomerID
+INNER JOIN [Order Details] od on od.OrderID = o.OrderID
+GROUP BY c.ContactName
+HAVING sum(od.UnitPrice) > 1000;
