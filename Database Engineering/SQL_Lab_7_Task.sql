10dbd7da08bda7c3ecd87eb14aed25c1cf4e4a9c
Serafim Lazuka <serafim.lazuko@gmail.com>
2019-02-26 01:05:49 +0300

    Add some files

---
+++ Database Engineering/SQL_Lab_7_Task.sql
@@ -0,0 +1,5 @@
+﻿SELECT CategoryName, COUNT(UnitsInStock) as [In Stock], SUM(p.UnitPrice) as Price
+FROM Products p
+INNER JOIN Categories c ON p.CategoryID = c.CategoryID
+GROUP BY c.CategoryName
+ORDER BY SUM(p.UnitPrice) asc;
---
+++ Database Engineering/SQL_Lab_8_Task.sql
@@ -0,0 +1,29 @@
+﻿ALTER Table Products
+Add PriceCategory varchar(20);
+
+UPDATE Products
+SET PriceCategory = 'Cheap' 
+WHERE CAST(UnitPrice AS decimal) BETWEEN 0 AND 20;
+
+UPDATE Products
+SET PriceCategory = 'Average'
+WHERE CAST(UnitPrice AS decimal) BETWEEN 21 AND 40;
+
+UPDATE Products
+SET PriceCategory = 'Expensive'
+WHERE CAST(UnitPrice AS decimal) > 40;
+
+Select ProductName, UnitPrice, PriceCategory
+FROM Products
+Where PriceCategory = 'Cheap'
+Order by UnitPrice;
+
+Select ProductName, UnitPrice, PriceCategory
+FROM Products
+Where PriceCategory = 'Average'
+Order by UnitPrice;
+
+Select ProductName, UnitPrice, PriceCategory
+FROM Products
+Where PriceCategory = 'Expensive'
+Order by UnitPrice;
